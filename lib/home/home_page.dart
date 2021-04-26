b3b7602e22e63305bfe0cac5cd43ca402d7c3a20
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-04-25 22:27:32 -0300

    Criação do QuizCard

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quizcard/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,22 +15,28 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBarWidget(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            LevelButtonWidget(
-              label: "Fácil",
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(
+                  label: "Fácil",
+                ),
+                LevelButtonWidget(
+                  label: "Médio",
+                ),
+                LevelButtonWidget(
+                  label: "Difícil",
+                ),
+                LevelButtonWidget(
+                  label: "Perito",
+                ),
+                QuizCardWidget(),
+              ],
             ),
-            LevelButtonWidget(
-              label: "Médio",
-            ),
-            LevelButtonWidget(
-              label: "Difícil",
-            ),
-            LevelButtonWidget(
-              label: "Perito",
-            ),
+            QuizCardWidget(),
           ],
         ),
       ),
